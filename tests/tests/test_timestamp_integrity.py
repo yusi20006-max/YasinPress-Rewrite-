c7dac3f6f7cf31315a61cfc070aa31d467b40f1c
yusi20006-max <yusi20006@gmail.com>
2026-08-20 16:50:55 +0330

    chore: remove duplicate tests/tests/ directory

--- tests/tests/test_timestamp_integrity.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timezone
-from yasinpress.core.helpers import format_persian_datetime
-
-
-def test_timestamp_integrity_basic():
-    dt = datetime(2026, 8, 11, 14, 30, tzinfo=timezone.utc)
-    out = format_persian_datetime(dt)
-    assert " - " in out
-    assert len(out.split(" - ")) == 2
